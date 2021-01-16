78d5d5314a87ed7235ee8c7ac1e3c231e99742e1
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2021-01-16 20:17:00 +0200

    stan::model::model_functional specialization for model_base_interface

--- src/stan/model/model_functional.hpp
+++ src/stan/model/model_functional.hpp
@@ -2,13 +2,14 @@
 #define STAN_MODEL_MODEL_FUNCTIONAL_HPP
 
 #include <stan/math/prim/fun/Eigen.hpp>
+#include <stan/model/model_base_interface.hpp>
 #include <iostream>
 
 namespace stan {
 namespace model {
 
 // Interface for automatic differentiation of models
-template <class M>
+template <class M, typename Enable = void>
 struct model_functional {
   const M& model;
   std::ostream* o;
@@ -20,6 +21,21 @@ struct model_functional {
     // log_prob() requires non-const but doesn't modify its argument
     return model.template log_prob<true, true, T>(
         const_cast<Eigen::Matrix<T, -1, 1>&>(x), o);
+  }
+};
+
+// model_functional is not supported to work with derived class of
+// model_base_interface
+template <class M>
+struct model_functional<M, enable_if_derived_interface_t<M>> {
+  const M& model;
+  std::ostream* o;
+
+  model_functional(const M& m, std::ostream* out) : model(m), o(out) {
+    std::ostringstream error_msg;
+    error_msg << "model_functional is not supported to work with '"
+              << typeid(model).name() << "'";
+    throw std::runtime_error(error_msg.str());
   }
 };
 
--- src/test/unit/model/model_functional_test.cpp
+++ src/test/unit/model/model_functional_test.cpp
@@ -1,2 +1,17 @@
 #include <stan/model/model_functional.hpp>
+#include <test/unit/model/test_model_interface_rosenbrock.hpp>
 #include <gtest/gtest.h>
+
+TEST(ModelUtil, model_functionalModelBaseInterface) {
+  int dim = 2;
+  rosenbrock_model model(dim);
+
+  EXPECT_THROW(stan::model::model_functional<rosenbrock_model>(model, 0),
+               std::runtime_error);
+
+  std::stringstream out;
+  out.str("");
+  EXPECT_THROW(stan::model::model_functional<rosenbrock_model>(model, &out),
+               std::runtime_error);
+  EXPECT_EQ("", out.str());
+}
